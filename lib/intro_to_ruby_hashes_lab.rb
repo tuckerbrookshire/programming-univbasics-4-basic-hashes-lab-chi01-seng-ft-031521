3de5de6650bfc45ee034cf6062f23fdb6f18f5ac
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-09 20:49:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,10 +8,9 @@ end
 
 def pioneer
   {:name => "Grace Hopper"}
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {:id => (number)}
 end
